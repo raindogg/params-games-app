21de7b7e7165a14d412f61392b1312d7cf09c7c1
wabray17 <wabray17@gmail.com>
2016-08-30 19:38:44 -0500

    Updated to allow for nil params

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,16 +1,39 @@
 class GamesController < ApplicationController
   def name
-    @name = params[:name].upcase
+    @name = params[:name]
+    if @name != nil
+      @message = 'NAME GAME LOVES ' + @name.upcase + '!'
+    else
+      @message = 'NAME GAME DOES NOT LOVE YOU.' 
+    end
   end
 
   def first
     @name = params[:name]
-    letter = @name.split('')
-    @first_letter = letter[0]
+    if @name != nil
+      @first_letter = @name.first
+      if @first_letter.downcase == 'a'
+        @message = 'Hey, your name starts with the first name of the alphabet!'
+      else
+        @message = 'Why hello there ' + @name 
+      end
+    else 
+      @message = 'Hi there! If you put your name up there as a parameter I will give you a message.'
+    end
   end
 
   def number
     @correct_number = 17
     @user_number = params[:number].to_i
+    if @user_number.nil?
+      @message = 'I want to play a game. Pass a number as a params.'
+    elsif @user_number > @correct_number
+      @message = 'Your guess is too high.'
+    elsif @user_number < @correct_number
+      @message = 'Your guess is too low.'
+    elsif @user_number == @correct_number
+      @message = 'You win!'
+    end
   end
 end
+
